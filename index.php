4f8925e97bcf3685b8c74435e38dbe06487e4a83
Gubagoo-Nathan-Nguyen <nathan.nguyen@gubagoo.com>
2024-04-16 17:19:16 -0500

    added header page

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portolio Website</title>
+
+    <!-- font cdl link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section -->
+
+<header class="header">
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <a href="#home" class="logo">Portfolio.</a>
+
+    <nav class="navbar">
+        <a href="#home" class="active">home</a>
+        <a href="#about">about</a>
+        <a href="#services">services</a>
+        <a href="#portfolio">portfolio</a>
+        <a href="#contact">contact</a>
+    </nav>
+
+    <!-- social media stuff -->
+    <div class="follow">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-github"></a>
+    </div>
+
+
+
+
+
+
+</header>
+
+<!-- header section end -->
+
+<!-- home section -->
+
+<section class="home" id="home">
+    <div class="image">
+        <img src="images/user-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>introduction message</h3>
+        <span>position</span>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio culpa nemo reiciendis dolores consequatur numquam?</p>
+        <a href="#about" class="btn">about me</a>
+    </div>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+
+</body>
+</html>
